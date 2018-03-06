ce49b8a8eccc33e984660a644f339d89f2f96201
Jakob <jakob@eggerapps.at>
2018-03-06 16:16:42 +0100

    Initial Commit

---
+++ Bonjour Menu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bonjour Menu
+//
+//  Created by Jakob Egger on 2018-03-06.
+//  Copyright © 2018 Egger Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
