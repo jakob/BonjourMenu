9e8f2031af276e942ddb0ac6b439193ba27a103c
Jakob <jakob@eggerapps.at>
2018-03-06 23:12:03 +0100

    Add Support For Multiple Services

--- Bonjour Menu/AppDelegate.swift
+++ Bonjour Menu/AppDelegate.swift
@@ -20,54 +20,73 @@ class AppDelegate: NSObject, NSApplicationDelegate, NetServiceBrowserDelegate, N
     let statusMenu = NSMenu()
     let statusIcon = NSImage(named: .statusIcon)
 
-    var netservices = [NetService]();
+    let serviceTypes = ["_postgresql._tcp.", "_http._tcp."]
+    let serviceTypeNames = ["PostgreSQL", "Web Sites"]
+    var serviceBrowsers = [NetServiceBrowser]()
+    var netservices = [[NetService]]()
+    var servicesForMenuItems = [NSMenuItem: NetService]()
     
     var netServiceToOpen: NetService?
-    
-    let netServiceBrowser = NetServiceBrowser()
     
     @IBOutlet weak var window: NSWindow!
 
     func applicationWillFinishLaunching(_ notification: Notification) {
         statusItem.menu = statusMenu
         statusItem.image = statusIcon
-        updateMenu()
         
-        netServiceBrowser.delegate = self
-        netServiceBrowser.searchForServices(ofType: "_postgresql._tcp", inDomain: "local.")
+        for serviceType in serviceTypes {
+            let browser = NetServiceBrowser()
+            browser.delegate = self
+            browser.searchForServices(ofType: serviceType, inDomain: "local.")
+            serviceBrowsers.append(browser)
+            netservices.append([])
+        }
+        
+        updateMenu()
     }
     
     func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
-        netservices.append(service)
+        guard let i = serviceBrowsers.index(of: browser) else { return }
+        netservices[i].append(service)
         service.delegate = self
         if !moreComing { updateMenu() }
     }
     
     func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
-        if let index = netservices.index(of: service) {
-            netservices.remove(at: index)
+        guard let i = serviceBrowsers.index(of: browser) else { return }
+        if let index = netservices[i].index(of: service) {
+            netservices[i].remove(at: index)
         }
         if !moreComing { updateMenu() }
     }
     
     func updateMenu() {
         statusMenu.removeAllItems()
+        servicesForMenuItems.removeAll()
         
-        if netservices.isEmpty {
-            let loadingItem = NSMenuItem(title: "No Services Found", action: nil, keyEquivalent: "")
-            statusMenu.addItem(loadingItem)
-        } else {
-            for service in netservices {
-                statusMenu.addItem(withTitle: service.name, action: #selector(AppDelegate.connectToService(_:)), keyEquivalent: "")
+        for i in serviceTypes.indices {
+            let titleItem = NSMenuItem(title: serviceTypeNames[i], action: nil, keyEquivalent: "")
+            titleItem.isEnabled = false
+            statusMenu.addItem(titleItem)
+            if netservices[i].isEmpty {
+                let loadingItem = NSMenuItem(title: "No Services Found", action: nil, keyEquivalent: "")
+                loadingItem.isEnabled = false
+                statusMenu.addItem(loadingItem)
+            } else {
+                for service in netservices[i] {
+                    let item = NSMenuItem(title: service.name, action: #selector(AppDelegate.connectToService(_:)), keyEquivalent: "")
+                    servicesForMenuItems[item] = service
+                    statusMenu.addItem(item)
+                }
             }
+            statusMenu.addItem(NSMenuItem.separator())
         }
-        statusMenu.addItem(NSMenuItem.separator())
         let quitItem = NSMenuItem(title: "Quit Bonjour Menu", action: #selector(NSApplication.terminate), keyEquivalent: "")
         statusMenu.addItem(quitItem)
     }
     
     @objc func connectToService(_ sender: NSMenuItem) {
-        let service = netservices[sender.menu!.index(of: sender)]
+        guard let service = servicesForMenuItems[sender] else { NSSound.beep(); return }
         netServiceToOpen = service
         service.delegate = self
         service.resolve(withTimeout: 3)
@@ -75,8 +94,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NetServiceBrowserDelegate, N
     
     func netServiceDidResolveAddress(_ sender: NetService) {
         guard netServiceToOpen == sender else { return }
+        let scheme = sender.type.dropLast(6).dropFirst()
+        let qs = scheme == "postgresql" ? "?nickname=\(sender.name.addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed)!)" : "";
         guard let hostname = sender.hostName?.trimmingCharacters(in: CharacterSet(charactersIn: ".")) else { NSSound.beep(); return }
-        guard let url = URL(string:"postgresql://\(hostname):\(sender.port)?nickname=\(sender.name.addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed)!)") else { NSSound.beep(); return }
+        guard let url = URL(string:"\(scheme)://\(hostname):\(sender.port)\(qs)") else { NSSound.beep(); return }
         print(url)
         NSWorkspace.shared.open(url)
     }
